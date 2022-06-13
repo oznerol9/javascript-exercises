733d376b4025cddd3403f0edd715acf466f02166
oznerol9 <lo.ludovici@gmail.com>
2022-06-14 01:29:35 +0200

    solutions to first seven problems

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(num) {
+	// [°C] = ([°F] − 32) × 5⁄9
+	return Math.round((num - 32) * (5/9) *10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(num) {
+	// [°F] = [°C] × 9⁄5 + 32
+	return Math.round(((num * (9/5)) + 32)*10)/10;
 };
 
 // Do not edit below this line
