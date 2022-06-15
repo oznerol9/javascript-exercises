d482a5f7437a8b8b70b695dc0a024413e7e19dd0
oznerol9 <lo.ludovici@gmail.com>
2022-06-15 11:58:34 +0200

    do more exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((previousValue, currentValue) => previousValue + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	return array.reduce((accumulator, nextItem) => accumulator * nextItem);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
+const factorial = function(num) {
+
+	var result = num;
 	
+	if (num === 0 || num === 1) {
+		return 1	
+	}
+	while (num > 1) {
+		num--;
+		result *= num;
+	}
+	return result;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+	processedString = str.toLowerCase().replace(/[\W_]/g, "");
+	return processedString.split("").reverse().join("") == processedString;
 };
 
 // Do not edit below this line
